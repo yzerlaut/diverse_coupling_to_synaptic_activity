6e0e02bf5e9368630a7efdb20bdcd8cdd12b30a8
yzerlaut <zerlaut@unic.cnrs-gif.fr>
2016-01-11 21:44:23 +0100

    up nb

--- input_impedance_calibration/plot_all_data.py
+++ input_impedance_calibration/plot_all_data.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pylab as plt
 import os, sys
 sys.path.append('/home/yann/work/python_library/')
-from my_graph import set_plot
+import my_graph as graph
 
 fig, ax = plt.subplots(figsize=(9,7))
 MICE, RATS = [], []
+
+psd_boundaries = [100,1000]
 
 for file in os.listdir("./intracellular_data/"):
     if file.endswith("_rat.txt"):
@@ -14,8 +16,13 @@ for file in os.listdir("./intracellular_data/"):
     elif file.endswith(".txt"):
         freq, psd, phase = np.loadtxt("./intracellular_data/"+file, unpack=True)
         MICE = {'freq':freq, 'psd':psd, 'phase':phase}
+    psd_boundaries[0] = min([psd_boundaries[0], psd.min()])
+    psd_boundaries[1] = max([psd_boundaries[1], psd.max()])
 
-        
+mymap = graph.get_linear_colormap()
+X = [100,200,500,1000]
+
+for m in 
 plt.loglog(freq, psd/psd.max(), 'rD')
 plt.loglog(freq, psd/psd.max(), 'bD')
 plt.show()
