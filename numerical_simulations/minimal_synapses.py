972653e05af4fb0cd4bdf8cc6d535daf3a59bbcd
yzerlaut <yann.zerlaut@gmail.com>
2016-09-21 15:41:12 +0200

    adding minimial synapses examples

---
+++ numerical_simulations/minimal_synapses.py
@@ -0,0 +1,17 @@
+from neuron import h as nrn
+import numpy as np
+import matplotlib.pylab as plt
+
+
+soma = nrn.Section()
+soma.insert('pas')
+soma.L, soma.diam = 58, 58
+soma.g_pas, soma.e_pas = 5e-5, -70.
+syn = nrn.glutamateLarkum2009(0.5, sec=soma)
+netcon = nrn.NetCon(nrn.nil, syn)
+netcon.weight[0] = 3e-9*1e6
+
+
+
+
+
